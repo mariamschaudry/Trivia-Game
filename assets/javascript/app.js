c8057bf4e2c35e3b3128a08c5da8b63fae1fea61
Mariam Chaudry <mariamschaudry1@gmail.com>
2018-03-16 23:12:24 -0400

    updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -36,6 +36,9 @@
 
 // Code for game begins here: 
 
+// Variables//
+
+
 // Creating and appending the start button //
 $(document).ready(function() {
 
@@ -49,100 +52,150 @@ startScreen();
 
 // Creating the on-click event for the start button which will lead to the 1st question once clicked //
 
-$(document).on('click','.start-button',function(event) {
+$("body").on('click','.start-button',function(event) {
+    //event.preventDefault(); 
+    console.log("button clicked")
     loadQuestions(); 
+
+});
+
+$("body").on('click','.answer-button',function(event) {
+    //event.preventDefault(); 
+    console.log("answer button clicked")
+
+    if (questionNumber < 9) {
+        questionNumber++;
+    }
+    
+    //else (resetGame)
+
+    loadQuestions(); 
+
+});
 
 
 // 30 second timer: starts at 30 seconds; counts down by 1 second; if it gets to zero then clear it out, if above zero then display that time. //
 
-var timerBox = setInterval(decrement, 1000);
+var timerBox = setInterval(1000);
 var timer = 30; 
+var clock;
 
-function thirtySeconds () {
-    if (timer === 0) {
-        clearInterval (timerBox);
-    }
+function wait() {
+	if (questionNumber < 9) {
+
+	questionNumber++;
      
-    if (timer > 0) {
-        timer --;
+    loadQuestions();
+    
+    timer = 30;
+        
+	answerClock();
+    
     }
-
-    $(".timer").html(timer);
+	//else {
+		//finalScreen();
+//	}
 }
+
+function answerClock() {
+    clock = setInterval(thirtySeconds, 1000);
+    
+    function thirtySeconds() {
+        
+        if (timer === 0) {
+
+            clearInterval(clock);
+            
+			//generateLossDueToTimeOut();
+        }
+        
+		if (timer > 0) {
+			timer--;
+		}
+		$(".timerBox").html(timer);
+	}
+}
+
+var questionNumber = 0;
+var questionArray = [
+    { 
+        question: "What is the most popular spice in the world?",
+        options: ['a. pepper', 'b. cloves', 'c. paprika', 'd. thyme'],
+        answer: "a. pepper"
+        
+    },
+    {
+        question: "The Cajun holy trinity of cooking consists of what three vegetables?",
+        options: ["a. tomatoes, corn, bell peppers", "b. bell peppers, carrots, celery", "c. celery, onions, bell peppers", "d. onions, tomatoes, corn"],
+        answer: "c. celery, onions, bell peppers"
+        
+    },
+    {
+        question: "Sriracha is a type of hot sauce named after a city located in which country?",
+        options: ['a. Japan', 'b. Mexico', 'c. Italy', 'd. Thailand'],
+        answer: "d. Thailand"
+        
+    },
+    {
+        question: "Malbec, Sangiovese, and Syrah are all a type of what?",
+        options: ['a. cheese', 'b. fruit', 'c. wine', 'd. coffee'],
+        answer: "c. wine"
+        
+    },
+    {
+        question: "Sushi is a type of cuisine that originated in which country?",
+        options: ['a. China', 'b. Korea', 'c. Japan', 'd. Malaysia'],
+        answer: "c. Japan"
+        
+    },
+    {
+        question: "The first Starbucks was established in 1971 in which U.S. city?",
+        options: ['a. New York', 'b. Seattle', 'c. Miami', 'd. Atlanta'],
+        answer: "b. Seattle"
+        
+    },
+    {
+        question: "A tandoor is a type of what?",
+        options: ['a. oven', 'b. bread', 'c. fruit', 'd. coffee'],
+        answer: "a. oven"
+           
+    },
+    {
+        question: "Kopi luwak is a very expensive type of what?",
+        options: ['a. coffee', 'b. beer', 'c. cake', 'd. vegetable'],
+        answer: "a. coffee"
+        
+    },
+    {
+        question: "Bubble tea originate in which country?",
+        options: ['a. Korea', 'b. Spain', 'c. Antartica', 'd. Vietnam'],
+        answer: "d. Vietnam"
+        
+    },
+    {
+        question: "What is the main ingredient in a lassi (a popular drink in Pakistan)?",
+        options: ['a. yoghurt', 'b. soda', 'c. water', 'd. espresso'],
+        answer: "a. yoghurt"
+        
+    }
+    
+];
 
 // Dynamically loading the questions to the main game play area for user to play //
 
 var gameQuestions;
 
-// function loadQuestions () {
-   //gameQuestions =  
+function loadQuestions () {
 
+   gameQuestions = "<p class = 'text-center timerBox'>Time Remaining: <span class='timerBox'>30</span></p>" + questionArray[questionNumber].question + 
+   
+   "<p class='text-center main-button-container'><a class='btn btn-default btn-md btn-block answer-button' href='#' role='button'>" + questionArray[questionNumber].options[0] + "</a></p>" +
+   "<p class='text-center main-button-container'><a class='btn btn-default btn-md btn-block answer-button' href='#' role='button'>" + questionArray[questionNumber].options[1] + "</a></p>" +
+   "<p class='text-center main-button-container'><a class='btn btn-default btn-md btn-block answer-button' href='#' role='button'>" + questionArray[questionNumber].options[2] + "</a></p>" +
+   "<p class='text-center main-button-container'><a class='btn btn-default btn-md btn-block answer-button' href='#' role='button'>" + questionArray[questionNumber].options[3] + "</a></p>"
 
+   $(".titleAndStart").html(gameQuestions);
 
-var questionArray = [
-    { 
-        question1: "What is the most popular spice in the world?",
-        options: ['a. pepper', 'b. cloves', 'c. paprika', 'd. thyme'],
-        answer: "a. pepper",
-        correct: ""
-    },
-    {
-        question2: "The Cajun holy trinity of cooking consists of what three vegetables?",
-        options: ["a. tomatoes, corn, bell peppers', 'b. bell peppers, carrots, celery', 'c. celery, onions, bell peppers', 'd. onions, tomatoes, corn"],
-        answer: "c. celery, onions, bell peppers",
-        correct: ""
-    },
-    {
-        question3: "Sriracha is a type of hot sauce named after a city located in which country?",
-        options: ['a. Japan', 'b. Mexico', 'c. Italy', 'd. Thailand'],
-        answer: "d. Thailand",
-        correct: ""
-    },
-    {
-        question4: "Malbec, Sangiovese, and Syrah are all a type of what?",
-        options: ['a. cheese', 'b. fruit', 'c. wine', 'd. coffee'],
-        answer: "c. wine",
-        correct: ""
-    },
-    {
-        question5: "Sushi is a type of cuisine that originated in which country?",
-        options: ['a. China', 'b. Korea', 'c. Japan', 'd. Malaysia'],
-        answer: "c. Japan",
-        correct: ""
-    },
-    {
-        question6: "The first Starbucks was established in 1971 in which U.S. city?",
-        options: ['a. New York', 'b. Seattle', 'c. Miami', 'd. Atlanta'],
-        answer: "b. Seattle",
-        correct: ""
-    },
-    {
-        question7: "A tandoor is a type of what?",
-        options: ['a. oven', 'b. bread', 'c. fruit', 'd. coffee'],
-        answer: "a. oven",
-        correct: ""   
-    },
-    {
-        question8: "Kopi luwak is a very expensive type of what?",
-        options: ['a. coffee', 'b. beer', 'c. cake', 'd. vegetable'],
-        answer: "a. coffee",
-        correct: ""
-    },
-    {
-        question9: "Bubble tea originate in which country?",
-        options: ['a. Korea', 'b. Spain', 'c. Antartica', 'd. Vietnam'],
-        answer: "d. Vietnam",
-        correct: ""
-    },
-    {
-        question10: "What is the main ingredient in a lassi (a popular drink in Pakistan)?",
-        options: ['a. yoghurt', 'b. soda', 'c. water', 'd. espresso'],
-        answer: "a. yoghurt",
-        correct: ""
-    }
-    
-];
-
-    });
+}
 
 });
